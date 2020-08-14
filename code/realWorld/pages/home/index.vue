2907617df56cef1f3b4c28d07bd3dd76798335af
Ting.ye <ting.ye@MacBook-Pro.local>
2020-08-14 11:07:51 +0800

    push

--- code/realWorld/pages/home/index.vue
+++ code/realWorld/pages/home/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home-page">
-
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">conduit</h1>
@@ -10,7 +9,6 @@
 
     <div class="container page">
       <div class="row">
-
         <div class="col-md-9">
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
@@ -23,31 +21,68 @@
             </ul>
           </div>
 
-          <div class="article-preview"  v-for="article in articles" :key="article.slug">
+          <div
+            class="article-preview"
+            v-for="article in articles"
+            :key="article.slug"
+          >
             <div class="article-meta">
-              <nuxt-link class="author" :to="`/profile/${article.author.username}`" >
+              <nuxt-link
+                class="author"
+                :to="`/profile/${article.author.username}`"
+              >
                 <img :src="article.author.image" />
               </nuxt-link>
               <div class="info">
-                <nuxt-link class="author" :to="`/profile/${article.author.username}`" >
-                    {{ article.author.username }}
+                <nuxt-link
+                  class="author"
+                  :to="`/profile/${article.author.username}`"
+                >
+                  {{ article.author.username }}
                 </nuxt-link>
                 <span class="date">{{ article.createdAt }}</span>
               </div>
-             <button class="btn btn-outline-primary btn-sm pull-xs-right" :class="{ active: article.favorited }" >
-                <i class="ion-heart"></i> {{ article.favoritesCount }} 
+              <button
+                class="btn btn-outline-primary btn-sm pull-xs-right"
+                :class="{ active: article.favorited }"
+              >
+                <i class="ion-heart"></i> {{ article.favoritesCount }}
               </button>
             </div>
-            <nuxt-link :to="{ name: 'article', params: { slug: article.slug } }" class="preview-link" >
-                <h1>{{ article.title }}</h1>
-                <p>{{ article.description }}</p> 
-                <span @click="$router.push({ name: 'article', params: { slug: article.slug } })" >Read more...</span> 
+            <nuxt-link
+              :to="{ name: 'article', params: { slug: article.slug } }"
+              class="preview-link"
+            >
+              <h1>{{ article.title }}</h1>
+              <p>{{ article.description }}</p>
+              <span
+                @click="
+                  $router.push({
+                    name: 'article',
+                    params: { slug: article.slug },
+                  })
+                "
+                >Read more...</span
+              >
             </nuxt-link>
           </div>
-
         </div>
 
         <div class="col-md-3">
+          <nav>
+            <ul class="pagination">
+              <li class="page-item active">
+                <nuxt-link class="page-link" to="/">1</nuxt-link>
+              </li>
+              <li class="page-item">
+                <nuxt-link class="page-link" to="/">2</nuxt-link>
+              </li>
+              <li class="page-item">
+                <nuxt-link class="page-link" to="/">3</nuxt-link>
+              </li>
+            </ul>
+          </nav>
+
           <div class="sidebar">
             <p>Popular Tags</p>
 
@@ -63,28 +98,37 @@
             </div>
           </div>
         </div>
-
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import {getArticles} from '@/apis/acrticle'
+import { getArticles } from "@/apis/acrticle.js";
 export default {
-    name: 'Home',
-    async asyncData() {
-        const { data } = await getArticles()
-      console.log(data)
-      return {
-        articles: data.articles,
-        articlesCount: data.articlesCount
-      }
+  name: "Home",
+  async asyncData({ query }) {
+    const page = Number.parseInt(query.page || 1);
+    const limit = 20;
+    const { data } = await getArticles({
+      limit,
+      offset: (page - 1) * limit,
+    });
+    console.log(data);
+    return {
+      articles: data.articles,
+      articlesCount: data.articlesCount,
+      limit,
+      page,
+    };
+  },
+  computed: {
+    totalPage() {
+      return Math.ceil(this.articlesCount / this.limit);
     },
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
